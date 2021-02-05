f2687bbd031f5522c69f7e73d618afba891b5707
vishnuadepu82 <vishnu.adepu82@gmail.com>
2021-02-05 13:05:48 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,26 @@ import { LoanService } from "./loan.service";
 import { BuiltInPipesComponent } from "../built-in-pipes/built-in-pipes.component";
 import { InWordsPipe } from "./in-words.pipe";
 import { StructuralDirectivesDemoComponent } from "../structural-directives-demo/structural-directives-demo.component";
+import { MenuBarComponent } from "../menu-bar/menu-bar.component";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/loan", pathMatch: "full" },
+  { path: "loan", component: LoanFormComponent },
+  { path: "pipes", component: BuiltInPipesComponent },
+  { path: "sddc", component: StructuralDirectivesDemoComponent }
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   declarations: [
     AppComponent,
     HelloComponent,
     LoanFormComponent,
     BuiltInPipesComponent,
     InWordsPipe,
-    StructuralDirectivesDemoComponent
+    StructuralDirectivesDemoComponent,
+    MenuBarComponent
   ],
   bootstrap: [AppComponent],
   providers: [LoanService]
---
+++ src/menu-bar/menu-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-menu-bar",
+  templateUrl: "./menu-bar.component.html",
+  styleUrls: ["./menu-bar.component.css"]
+})
+export class MenuBarComponent implements OnInit {
+  links: string[][];
+  constructor() {
+    this.links = [
+      ["/", "Home"],
+      ["/loan", "Loan Form"],
+      ["/pipes", "Pipes Demo"],
+      ["/sddc", "Structural Directives Demo"]
+    ];
+  }
+
+  ngOnInit() {}
+}
