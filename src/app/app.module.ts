64d76a6f7ad4ef0dabe6aece5e52e545ebea1365
vishnuadepu82 <vishnu.adepu82@gmail.com>
2021-02-05 12:45:54 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { LoanFormComponent } from "./loan-form/loan-form.component";
-import { LoanService } from './loan.service';
+import { LoanService } from "./loan.service";
 import { BuiltInPipesComponent } from "../built-in-pipes/built-in-pipes.component";
-import { InWordsPipe } from './in-words.pipe';
+import { InWordsPipe } from "./in-words.pipe";
+import { StructuralDirectivesDemoComponent } from "../structural-directives-demo/structural-directives-demo.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, LoanFormComponent,BuiltInPipesComponent, InWordsPipe],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    LoanFormComponent,
+    BuiltInPipesComponent,
+    InWordsPipe,
+    StructuralDirectivesDemoComponent
+  ],
   bootstrap: [AppComponent],
   providers: [LoanService]
 })
