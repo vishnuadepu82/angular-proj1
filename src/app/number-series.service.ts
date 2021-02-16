cb65bf2817daa22477a843b7a1cd1432013769a4
vishnuadepu82 <vishnu.adepu82@gmail.com>
2021-02-16 14:36:55 +0530

    1 file modified

--- src/app/number-series.service.ts
+++ src/app/number-series.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable, Observer } from "rxjs";
+import { ObserveOnOperator } from "rxjs/internal/operators/observeOn";
 import { map, filter } from "rxjs/operators";
 
 @Injectable({
@@ -9,6 +10,47 @@ export class NumberSeriesService {
   constructor() {}
 
   generateSeries(lb: number, ub: number): Observable<number> {
-    return;
+    let job=(observer:Observer<number>)=>
+    {
+      if(lb>ub)
+      {
+        observer.error("Invalid Limits!");
+        return;
+      }
+
+      let n=lb;
+      let handle=setInterval(()=>{
+        observer.next(n);
+        n++;
+
+        if(n>ub)
+        {
+          observer.complete();
+          clearInterval(handle);
+        }
+      },2000);
+
+    };
+    return new Observable<number>(job);
   }
+
+  generateSquaredSeries(lb:number,ub:number) : Observable<number> {
+
+    let oldObservable = this.generateSeries(lb,ub);
+    let newObservable = oldObservable.pipe(map((n) => (n*n)));
+    return newObservable;
+  }
+  generateEvenSeries(lb:number,ub:number) : Observable<number> {
+
+    let oldObservable = this.generateSeries(lb,ub);
+    let newObservable = oldObservable.pipe(filter((n) => (n%2==0)));
+    return newObservable;
+  }
+generateEvenSquaredSeries(lb:number,ub:number) : Observable<number> {
+
+    let oldObservable = this.generateSeries(lb,ub);
+    let newObservable = oldObservable.pipe(filter((n) => (n%2==0)),map((n) => (n*n)));
+    return newObservable;
+  }
+
 }
